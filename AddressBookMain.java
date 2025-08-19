f01b796dcbfe3c832689ebf3eb97b79f8db151db
Honey R Shetty <honeyshetty2003@gmail.com>
2025-08-19 11:43:35 +0530

    second-commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,6 +2,12 @@ package address_book_system;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-    }
+		System.out.println("Welcome to Address Book Program");
+
+		Contact contact = new Contact("Honey", "Shetty", "MG Road", "Bangalore", "Karnataka", "560001", "9876543210",
+				"honey@example.com");
+
+		System.out.println("Contact Created:");
+		System.out.println(contact);
+	}
 }
